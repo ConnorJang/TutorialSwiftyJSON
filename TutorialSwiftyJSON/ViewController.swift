9a0af87e5cbc737b107a8edbf9e4777b8171aa26
Connor Jang <connorjang20@gmail.com>
2017-10-27 12:56:53 -0700

    Completed...

--- TutorialSwiftyJSON/ViewController.swift
+++ TutorialSwiftyJSON/ViewController.swift
@@ -15,29 +15,64 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let json = JSON(jsonData)
+        var json = JSON(jsonData)
         //print(json) --> Will print then json data in regular json format
         
         //Getting a string from a JSON Dictionary
-        let dict = json["dict"].stringValue
-        jsonTextBox.text = "The value from the 'dict' is \(dict)\n\n"
+        var dict = json["dict"].stringValue
+        jsonTextBox.text = "The value from the 'dict' is:\n     --->\(dict)\n\n"
         
         //Getting an array of numbers from a JSON Array
         let arrayVals =  json["array"]
-        jsonTextBox.text = jsonTextBox.text + "The values in 'array' are \(arrayVals[0]), and \(arrayVals[1])\n\n"
+        jsonTextBox.text = jsonTextBox.text + "The values in 'array' are:\n     --->\(arrayVals[0]), and \(arrayVals[1])\n\n"
         
         // Looping through values
         let users = json["users"]
         for i in 0...users.count-1 {
-            let text = ("User's name is \(users[i]["info"]["name"]), email is \(users[i]["info"]["email"])\n\n")
+            let text = ("User's name is \(users[i]["info"]["name"]), email is \(users[i]["info"]["email"])\n")
             jsonTextBox.text = jsonTextBox.text + text
         }
         //  Alternative, using path to element
         let path: [JSONSubscriptType] = ["users",1,"info","name"] // Path to the name jeff
         let name = json[path].stringValue
-        let nameText = ("You can use the a path to get a value, the path '['users',1,'info','name']' -- maps to \(name)")
+        let nameText = ("\nYou can use the a path to get a value, the path '['users',1,'info','name']' -- maps to \(name)\n\n")
         jsonTextBox.text = jsonTextBox.text + nameText
         
+        
+        // Errors: SwiftyJSON will prevent your app from crashing due to errors like "index out-of-bounds"
+        if let outOfBounds = json["array"][9999].string {
+            // Try to print
+            print(outOfBounds)
+        } else {
+            // Print the error
+            let errorText = String(describing: json["array"][9999].error)
+            jsonTextBox.text = jsonTextBox.text + "Index out-of-bounds error on json[array][9999]: " +  errorText + "\n\n"
+        }
+        
+        // Setting new values
+        json["dict"] = JSON("New string value")
+        dict = json["dict"].stringValue
+        jsonTextBox.text = jsonTextBox.text + "The NEW value set for the 'dict' is:\n     -->'\(dict)'\n\n"
+        
+        
+        // Merging json into another json
+        // If writing both contain same key, the original value is overwritten
+        //     If both are arrays with the same key, the new values get appended to the original
+        let update: JSON = [
+            "array": [11.11, 22.22, 33.33, 44.44, 55.55], // merges with original array
+            "update" : "merged from second json",   // add a new key/value
+            ]
+        do {
+            try json.merge(with: update)
+            var updatedJson = "Array with new merged values:\n     "
+            for i in 0...json["array"].count-1 {
+                updatedJson += "\(json["array"][i]), "
+            }
+            jsonTextBox.text = jsonTextBox.text + updatedJson + "\n\n"
+            jsonTextBox.text = jsonTextBox.text + "Key/Value 'update' merged in as new json value:\n     -->'\(json["update"])'"
+        } catch {
+            print(error)
+        }
         
         
     }
@@ -47,6 +82,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBOutlet weak var jsonTextBox: UITextView!
+    
+    
     
     var jsonData: JSON = [
         "dict" : "SomeDictionaryStringValue",
